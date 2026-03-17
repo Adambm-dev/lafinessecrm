615351ba13a4037f3c2fde86efae83bd64ed75bb
Adambm-dev <medadambenmoussa@outlook.com>
2026-03-17 10:30:50 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+const supabaseUrl = 'https://ahroejvzmollzbhwnuei.supabase.co'
+const supabaseAnonKey = 'sb_publishable_-CkVF-rLxdVMMixMdOrSAQ_5POndYu1'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
