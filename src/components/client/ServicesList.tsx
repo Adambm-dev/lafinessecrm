dcc1643b2a28f5f4baab6026ae1e1433e436c228
Adambm-dev <medadambenmoussa@outlook.com>
2026-03-17 12:13:08 +0100

    Update ServicesList.tsx

--- src/components/client/ServicesList.tsx
+++ src/components/client/ServicesList.tsx
@@ -9,15 +9,15 @@ export default function ServicesList() {
   }, []);
 
   return (
-    <div className="p-6">
+    <div>
       <h2 className="text-2xl mb-4 font-bold text-pink-600">Nos Services</h2>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {services.map(s => (
-          <div key={s.id} className="border p-3 rounded shadow-sm">
+          <div key={s.id} className="border p-4 bg-white rounded shadow">
             <b>{s.name}</b>
             <p>{s.duration} min</p>
-            <p className="text-pink-600 text-xl">{s.price}€</p>
+            <p className="text-pink-600">{s.price}€</p>
           </div>
         ))}
       </div>
