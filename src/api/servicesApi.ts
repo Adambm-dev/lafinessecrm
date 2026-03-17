fadb749b9829ee3ac8f11e02fb668df46a34684d
Adambm-dev <medadambenmoussa@outlook.com>
2026-03-17 12:01:37 +0100

    Update servicesApi.ts

--- src/api/servicesApi.ts
+++ src/api/servicesApi.ts
@@ -1,19 +1,16 @@
 import { supabase } from "../lib/supabase";
 
 export const ServicesApi = {
-  async getAll() {
+  getAll() {
     return supabase.from("services").select("*").order("name");
   },
-
-  async create(data: any) {
+  create(data: any) {
     return supabase.from("services").insert(data);
   },
-
-  async update(id: string, data: any) {
+  update(id: string, data: any) {
     return supabase.from("services").update(data).eq("id", id);
   },
-
-  async delete(id: string) {
+  delete(id: string) {
     return supabase.from("services").delete().eq("id", id);
   }
 };
